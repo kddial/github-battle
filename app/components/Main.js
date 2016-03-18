853b009fd89ef99ad214eb27274502d788229211
Kevin Dial <kevin.dial.phone@gmail.com>
2016-03-18 01:01:15 -0400

    added indexRoute. finished video 4.

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,7 +3,7 @@ var React = require('react');
 var Main = React.createClass({
   render: function() {
     return (
-      <div>
+      <div className='main-container'>
         <a href="./#/"> main </a>
         <a href="./#/home"> home </a>
         {this.props.children}
