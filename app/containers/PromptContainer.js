feaa5dd6052c3268cf1737b8bf4c282b9eff9225
Kevin Dial <kevin.dial.phone@gmail.com>
2016-03-18 07:48:56 -0400

    refactored into presentational component and container componenets. added PropTypes. used stateless functional componenets

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
   contextTypes: {
@@ -10,12 +10,12 @@ var PromptContainer = React.createClass({
       username: ''
     }
   },
-  onUpdateUser: function(event) {
+  handleUpdateUser: function(event) {
     this.setState({
       username: event.target.value
     });
   },
-  onSubmitUser: function(e) {
+  handleSubmitUser: function(e) {
     // prevent the default action of submit to occur
     e.preventDefault();
 
@@ -45,29 +45,11 @@ var PromptContainer = React.createClass({
   },
   render: function() {
     return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-        <h1> {this.props.route.header} </h1>
-        <div className="col-sm-12">
-          <form onSubmit={this.onSubmitUser}>
-            <div className="form-group">
-              <input
-                className="form-control"
-                placeholder="Github UserName"
-                type="text" 
-                onChange={this.onUpdateUser}
-                value={this.state.username}
-                />
-            </div>
-            <div className="form-group col-sm-4 col-sm-offset-4">
-              <button 
-                className="btn btn-block btn-success"
-                type="submit">
-                Continue
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt 
+        onSubmitUser={this.handleSubmitUser}
+        onUpdateUser={this.handleUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username} />
     )
   }
 });
